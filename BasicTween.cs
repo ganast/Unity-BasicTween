7117cdc223267b182325d3932055da3d372037af
ganast <ganast@ganast.com>
2019-12-08 01:11:52 +0200

    Comments, formatting.

--- BasicTween.cs
+++ BasicTween.cs
@@ -1,5 +1,5 @@
 ﻿/**
- * BasicTween v1.0 - A very simple tool for managing values (currently only
+ * BasicTween v1.2 - A very simple tool for managing values (currently only
  * floats) that need to be set directly, interpolated towards a target or
  * interpolated within a range easily and transparently.
  *
@@ -18,13 +18,17 @@
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * VERSION: 1.1.0, 20191128
+ * VERSION: 1.2.1, 20191208
  */
 
-using System; 
+using System;
 
 namespace com.ganast.Tween {
 
+    /// <summary>
+    /// A very simple tween library that manages a single variable. Relies on client-side time
+    /// updates and supports a variety of easing functions.
+    /// </summary>
     public class BasicTween {
 
         /// <summary>
@@ -37,21 +41,87 @@ namespace com.ganast.Tween {
         /// <returns></returns>
         public delegate float EasingFunction(float t, float b, float c, float d);
 
+        /// <summary>
+        /// Determines whether rate-of-change is interpreted as either interpolation speed or
+        /// duration.
+        /// </summary>
         public enum RateLogic {
             SPEED,
             DURATION
         }
 
+        /// <summary>
+        /// A constant to indicate lack of limit.
+        /// </summary>
         public const float UNLIMITED = float.NaN;
 
-        private float v, v0, v1, t, r, d, vmin, vmax;
+        /// <summary>
+        /// Value of the variable managed by this implementation.
+        /// </summary>
+        private float v;
 
+        /// <summary>
+        /// Starting value of tweening range.
+        /// </summary>
+        private float v0;
+
+        /// <summary>
+        /// Ending value of tweening range.
+        /// </summary>
+        private float v1;
+
+        /// <summary>
+        /// Interpolation time. Updated by client code when <see cref="Update(float)"/> is called.
+        /// </summary>
+        private float t;
+
+        /// <summary>
+        /// Rate-of-change. Represents either interpolation speed or duration, according to
+        /// <see cref="rateLogic"/>.
+        /// </summary>
+        private float r;
+
+        /// <summary>
+        /// Interpolation duration. Calculated every time a new tweening range is set depending on
+        /// rate-of-change and <see cref="rateLogic"/>.
+        /// </summary>
+        private float d;
+
+        /// <summary>
+        /// Minimum value for range-limited variables.
+        /// </summary>
+        private float vmin;
+
+        /// <summary>
+        /// Maximum value for range-limited variables.
+        /// </summary>
+        private float vmax;
+
+        /// <summary>
+        /// Easing function used for interpolation.
+        /// </summary>
         private EasingFunction easingFunction;
 
+        /// <summary>
+        /// Determines whether rate-of-change shall be interpreted as either intepolation
+        /// speed or duration.
+        /// </summary>
         private RateLogic rateLogic;
 
+        /// <summary>
+        /// Public-level API lock.
+        /// </summary>
         private object _lock = new object();
 
+        /// <summary>
+        /// Constructs a <see cref="BasicTween"/> for a range-limited variable.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="vmin"></param>
+        /// <param name="vmax"></param>
+        /// <param name="easingFunction"></param>
+        /// <param name="rateLogic"></param>
+        /// <param name="r"></param>
         public BasicTween(float v, float vmin, float vmax, EasingFunction easingFunction, RateLogic rateLogic, float r) {
             this.easingFunction = easingFunction;
             this.rateLogic = rateLogic;
@@ -61,35 +131,74 @@ namespace com.ganast.Tween {
             SetImmediate(v, false);
         }
 
-        public BasicTween(float v, EasingFunction easingFunction, RateLogic rateLogic, float r) : this(v, UNLIMITED, UNLIMITED, easingFunction, rateLogic, r) {
+        /// <summary>
+        /// Constructs a <see cref="BasicTween"/> for an non-limited variable.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="easingFunction"></param>
+        /// <param name="rateLogic"></param>
+        /// <param name="r"></param>
+        public BasicTween(float v, EasingFunction easingFunction, RateLogic rateLogic, float r) :
+            this(v, UNLIMITED, UNLIMITED, easingFunction, rateLogic, r) {
         }
 
+        /// <summary>
+        /// Returns the value of the managed variable.
+        /// </summary>
+        /// <returns>Value of the managed variable at the time of calling.</returns>
         public float GetValue() {
             return v;
         }
 
+        /// <summary>
+        /// Returns the interpolation time.
+        /// </summary>
+        /// <returns>Interpolation time at the time of calling.</returns>
         public float GetTime() {
             return t;
         }
 
+        /// <summary>
+        /// Immediately sets or changes the managed variable to or by the specified value.
+        /// </summary>
+        /// <param name="value">TODO</param>
+        /// <param name="relative">TODO</param>
         public void SetValue(float value, bool relative = false) {
             lock (_lock) {
                 SetImmediate(value, relative);
             }
         }
 
+        /// <summary>
+        /// Initiates tweening from the managed variable's current value to the specified absolute
+        /// or relative target value, after making sure the target value is within limits.
+        /// </summary>
+        /// <param name="target">TODO</param>
+        /// <param name="relative">TODO</param>
         public void SetTarget(float target, bool relative = false) {
             lock (_lock) {
                 SetInterpolation(target, relative);
             }
         }
 
+        /// <summary>
+        /// Immediately sets the managed variable to the specified origin value and initiates
+        /// tweening to the specified absolute or relative target value, after making sure the
+        /// entire tweening range is within limits.
+        /// </summary>
+        /// <param name="target">TODO</param>
+        /// <param name="origin">TODO</param>
+        /// <param name="relative">TODO</param>
         public void SetRange(float target, float origin, bool relative = false) {
             lock (_lock) {
                 SetInterpolation(target, origin, relative);
             }
         }
 
+        /// <summary>
+        /// Updates the value of the managed variable for the specified amount of elapsed time.
+        /// </summary>
+        /// <param name="dt">TODO</param>
         public void Update(float dt) {
 
             // Debug.LogFormat("v={0}, v0={1}, v1={2}, dt={3}, t={4}", v, v0, v1, Time.time - t, t);
@@ -100,7 +209,6 @@ namespace com.ganast.Tween {
 
                     t += dt;
 
-                    // TODO: get value from easing function...
                     v = easingFunction(t, v0, v1 - v0, d);
                 }
                 else {
@@ -110,32 +218,44 @@ namespace com.ganast.Tween {
         }
 
         private void SetImmediate(float value, bool relative = false) {
+
             if (relative) {
                 v += value;
             }
             else {
                 v = value;
             }
+
             v = Sanitize(v);
+
             v0 = v;
+
             v1 = v;
+
             t = 0.0f;
         }
 
         private void SetInterpolation(float target, bool relative = false) {
+
             v0 = v;
-            t = 0.0f;
+
             if (relative) {
                 v1 += target;
             }
             else {
                 v1 = target;
             }
+
             v1 = Sanitize(v1);
+
+            t = 0.0f;
+
             switch (rateLogic) {
+
                 case RateLogic.DURATION:
                     d = r;
                     break;
+
                 case RateLogic.SPEED:
                     d = Math.Abs(v1 - v) / r;
                     break;
@@ -148,12 +268,15 @@ namespace com.ganast.Tween {
         }
 
         public float Sanitize(float f) {
+
             if (!float.IsNaN(vmin) && f < vmin) {
                 return vmin;
             }
+
             else if (!float.IsNaN(vmax) && f > vmax) {
                 return vmax;
             }
+
             else {
                 return f;
             }
